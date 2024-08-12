49d18afb09923b1ce74f5c4d09916b0e218b2f31
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2024-08-12 17:42:57 -0300

    Correção bug botao, adc. requisito caso numero existente

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('formulario')
 const tabelaContatos = document.querySelector('tbody')
+const contatos = []
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
@@ -12,13 +13,20 @@ function adicionarContato(){
     const nomeContato = document.getElementById('nome-contato').value
     const numeroContato = document.getElementById('numero-contato').value
 
-    let linha = '<tr>'
-    linha += `<td>${nomeContato}</td>`
-    linha += `<td>${numeroContato}</td>`
-    linha += `</tr>`
+    if(contatos.includes(numeroContato)){
+        alert(`O numero:${numeroContato} ja foi adicionado a lista de contatos`)
+    }else {
+        contatos.push(numeroContato)
+        
+        let linha = '<tr>'
+        linha += `<td>${nomeContato}</td>`
+        linha += `<td>${numeroContato}</td>`
+        linha += `</tr>`
 
-    tabelaContatos.innerHTML += linha
+        tabelaContatos.innerHTML += linha
 
-    nomeContato = ''
-    numeroContato = ''
+        nomeContato = ''
+        numeroContato = ''        
+    }
+
 }
